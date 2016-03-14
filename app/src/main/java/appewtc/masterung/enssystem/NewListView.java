0ea19c8c4af603fe2c319e60a9c9dd891ce2a522
aom <dotpoly@gmail.com>
2016-03-14 11:10:39 +0700

    test

--- app/src/main/java/appewtc/masterung/enssystem/NewListView.java
+++ app/src/main/java/appewtc/masterung/enssystem/NewListView.java
@@ -3,13 +3,26 @@ package appewtc.masterung.enssystem;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
 
 public class NewListView extends AppCompatActivity {
+
+
+    //Explicit
+    private ListView newListView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_list_view);
+
+        //Bind Widdget
+        newListView = (ListView) findViewById(R.id.listView);
+
+
+
+
     }//Main Method
     public void clickBackNewsList(View view) {
          finish();
---
+++ app/src/main/java/appewtc/masterung/enssystem/NewsAdapter.java
@@ -0,0 +1,63 @@
+package appewtc.masterung.enssystem;
+
+import android.content.Context;
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by User on 14/3/2559.
+ */
+    public class NewsAdapter extends BaseAdapter {
+
+    //Explicit
+    private Context context;
+    private  String[]titleStrings, dateStrings, iconStrings;
+
+    public NewsAdapter(Context context, String[] titleStrings, String[] dateStrings, String[] iconStrings) {
+        this.context = context;
+        this.titleStrings = titleStrings;
+        this.dateStrings = dateStrings;
+        this.iconStrings = iconStrings;
+    }//Consturctor
+
+
+    @Override
+    public int getCount() {
+        return titleStrings.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+          View view1 = layoutInflater.inflate(R.layout.news_listview, viewGroup, false);
+            //Title
+        TextView titleTextView = (TextView) view1.findViewById(R.id.textView22);
+        titleTextView.setText(titleStrings[i]);
+        //Date
+            TextView dateTextView =(TextView) view1.findViewById(R.id.textView23);
+        dateTextView.setText(dateStrings[i]);
+
+        //Icon
+        ImageView iconImageView = (ImageView) view1.findViewById(R.id.imageView9);
+
+
+        return null;
+    }
+}// Main Class

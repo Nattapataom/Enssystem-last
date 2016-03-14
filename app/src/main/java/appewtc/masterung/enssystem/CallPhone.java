646afbd9deaf884dc51e97dddb9d65ab169b9ce7
aom <dotpoly@gmail.com>
2016-03-14 16:59:03 +0700

    call finishv1

--- app/src/main/java/appewtc/masterung/enssystem/CallPhone.java
+++ app/src/main/java/appewtc/masterung/enssystem/CallPhone.java
@@ -1,13 +1,54 @@
 package appewtc.masterung.enssystem;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 
 public class CallPhone extends AppCompatActivity {
+
+    //Explicit
+    private LinearLayout topLinearLayout;
+    private ListView phoneListView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_call_phone);
+
+        //Bind Widget
+        bindWidget();
+
+        //Show Choose
+        showChoose();
+
+        //Create ListView
+        createListView();
+
+
+    }   // Main Method
+
+    private void createListView() {
+        String[] callStrings = getIntent().getStringArrayExtra("Call");
+        String[] phoneStrings = getIntent().getStringArrayExtra("Phone");
+
+        PhoneAdapter phoneAdapter = new PhoneAdapter(CallPhone.this, callStrings, phoneStrings);
+        phoneListView.setAdapter(phoneAdapter);
+
+
+    }//createListView
+
+    private void showChoose() {
+        int intBackGround = getIntent().getIntExtra("Icon", R.drawable.catagory1);
+        topLinearLayout.setBackgroundResource(intBackGround);
     }
-}
+
+    private void bindWidget() {
+        topLinearLayout = (LinearLayout) findViewById(R.id.linTopCallPhone);
+        phoneListView = (ListView) findViewById(R.id.listView2);
+
+    }
+
+}   // Main Class

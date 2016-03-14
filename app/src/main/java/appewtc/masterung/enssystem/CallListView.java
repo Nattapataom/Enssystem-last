932db3502a08b2d2b829be779e241d1d053e5dce
aom <dotpoly@gmail.com>
2016-03-14 16:10:42 +0700

    call

--- app/src/main/java/appewtc/masterung/enssystem/CallListView.java
+++ app/src/main/java/appewtc/masterung/enssystem/CallListView.java
@@ -17,6 +17,18 @@ public class CallListView extends AppCompatActivity {
         setContentView(R.layout.activity_call_list_view);
 
         //Bind Widget
+    bindWidget();
 
     }//Main Method
+
+    private  void bindWidget(){
+        cat0ImageView = (ImageView)findViewById(R.id.imageView10);
+        cat1ImageView = (ImageView)findViewById(R.id.imageView11);
+        cat2ImageView = (ImageView)findViewById(R.id.imageView12);
+        cat3ImageView = (ImageView)findViewById(R.id.imageView13);
+        cat4ImageView = (ImageView)findViewById(R.id.imageView14);
+
+
+
+    }
 }//Main Class
